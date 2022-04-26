10545f671191039b53b3696f89338a3f70811cc5
Eduardo Fajardo <efmarcial1060@gmail.com>
2022-04-26 12:51:04 -0400

    staticfiles

--- akbuilder/settings.py
+++ akbuilder/settings.py
@@ -144,6 +144,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Extra places for collectstatics to find static files.
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media files 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
